862072d378bd809d81c804d78a2b25d444f3a61c
jaywcjlove <398188662@qq.com>
2018-03-09 17:36:36 +0800

    Add Mocker file hot replacement.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
-module.exports = function (app, proxy) {
+module.exports = function (app, watchFile) {
+  if (!watchFile) {
+    throw new Error('Mocker file does not exist!.');
+    return;
+  }
+  let proxy = require(watchFile);
   for (const key in proxy) {
     const general = key.toString().split(' ');
     if (general.length > 1 && general[0] && app[general[0].toLowerCase()]) {
@@ -19,6 +25,30 @@ module.exports = function (app, proxy) {
         });
       });
     }
+  }
+
+  // 监听文件修改重新加载代码
+  // 配置热更新
+  fs.watch(require.resolve(watchFile), function () {
+    try {
+      // 热更新先引用，冒烟，实时编辑报错，错误语法避免 crash
+      const moackData = require(watchFile);
+      // 确认没有问题进行热更新
+      cleanCache(require.resolve(watchFile));
+      // 完事儿，进行赋值，哈哈成功了！
+      proxy = require(watchFile);
+    } catch (ex) {
+      console.error('Hot Mocker file replacement failed!!');
+    }
+  });
+  // 释放老模块的资源
+  function cleanCache(modulePath) {
+    var module = require.cache[modulePath];
+    // remove reference in module.parent
+    if (module.parent) {
+      module.parent.children.splice(module.parent.children.indexOf(module), 1);
+    }
+    require.cache[modulePath] = null;
   }
   return function (req, res, next) {
     next();

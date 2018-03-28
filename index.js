9a53f5ad70a94a83c99f0d9c448db2f18ceef442
jaywcjlove <398188662@qq.com>
2018-03-28 15:27:10 +0800

    feat: Add mock api dynamic loaded.

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 const bodyParser = require('body-parser');
-const proxyMiddleware = require('http-proxy-middleware');
+const httpProxy = require('http-proxy');
 const fs = require('fs');
+
+const proxyHTTP = httpProxy.createProxyServer({});
+
+let proxy = {};
 
 module.exports = function (app, watchFile, proxyConf = {}) {
   if (!watchFile) {
     throw new Error('Mocker file does not exist!.');
     return;
   }
-  let proxy = require(watchFile);
-  for (const key in proxy) {
-    const general = key.toString().split(' ');
-    if (general.length > 1 && general[0] && app[general[0].toLowerCase()]) {
-      const proxyNames = Object.keys(proxyConf);
-      const proxyFuzzyMatch = proxyNames.filter(function (kname) {
-        return /\*$/.test(kname) && (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(key);;
+  proxy = require(watchFile);
+
+  app.all('/*', function (req, res, next) {
+    const proxyURL = `${req.method} ${req.path}`;
+    const proxyNames = Object.keys(proxyConf);
+    const proxyFuzzyMatch = proxyNames.filter(function (kname) {
+      return /\*$/.test(kname) && (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(proxyURL);;
+    });
+    const proxyMatch = proxyNames.filter(function (kname) {
+      return kname === proxyURL;
+    });
+
+    if (proxyNames.length > 0 && (proxyMatch.length > 0 || proxyFuzzyMatch.length > 0)) {
+      let currentProxy = proxyNames.filter(function (kname) {
+        return (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(proxyURL);;
       });
-      const proxyMatch = proxyNames.filter(function (kname) {
-        return kname === key;
-      });
-      if (proxyNames.length > 0 && (proxyMatch.length > 0 || proxyFuzzyMatch.length > 0)) {
-        let currentProxy = proxyNames.filter(function (kname) {
-          return (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(key);;
-        });
-        currentProxy = proxyConf[currentProxy[0]];
-        app[general[0].toLowerCase()](general[1], proxyMiddleware({ target: currentProxy, changeOrigin: true }));
-      } else {
-        app[general[0].toLowerCase()](general[1], function (req, res, next) {
-          const contentType = req.get('Content-Type');
-          let bodyParserMethd = bodyParser.json();
-          if (contentType === 'text/plain') {
-            bodyParserMethd = bodyParser.raw({ type: 'text/plain' });
-          }
-          bodyParserMethd(req, res, function () {
-            if (typeof proxy[key] === 'function') {
-              proxy[key](req, res, next);
-            } else {
-              res.json(proxy[key]);
-            }
-          });
-        });
+      currentProxy = proxyConf[currentProxy[0]];
+      proxyHTTP.web(req, res, { target: currentProxy });
+    } else if (proxy[proxyURL]) {
+      let bodyParserMethd = bodyParser.json();
+      const contentType = req.get('Content-Type');
+      if (contentType === 'text/plain') {
+        bodyParserMethd = bodyParser.raw({ type: 'text/plain' });
       }
+      bodyParserMethd(req, res, function () {
+        if (typeof proxy[proxyURL] === 'function') {
+          proxy[proxyURL](req, res, next);
+        } else {
+          res.json(proxy[proxyURL]);
+        }
+      });
+    } else {
+      next();
     }
-  }
+  });
 
   // 监听文件修改重新加载代码
   // 配置热更新
-  fs.watch(require.resolve(watchFile), function () {
+  fs.watch(require.resolve(watchFile), function (dt) {
     try {
       // 热更新先引用，冒烟，实时编辑报错，错误语法避免 crash
       const moackData = require(watchFile);
@@ -53,6 +57,7 @@ module.exports = function (app, watchFile, proxyConf = {}) {
       cleanCache(require.resolve(watchFile));
       // 完事儿，进行赋值，哈哈成功了！
       proxy = require(watchFile);
+      console.log(`Hot Mocker ${watchFile} file replacement success!`);
     } catch (ex) {
       console.error('Hot Mocker file replacement failed!!');
     }

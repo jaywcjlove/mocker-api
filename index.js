de58977f52fcfc2c3f11e6fb64e46a99f17ee5f4
jaywcjlove <398188662@qq.com>
2018-03-28 18:20:19 +0800

    Fixed bug where routing and routing parameters match.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const httpProxy = require('http-proxy');
 const fs = require('fs');
+require('colors-cli/toxic');
 
 const proxyHTTP = httpProxy.createProxyServer({});
 
@@ -13,39 +14,11 @@ module.exports = function (app, watchFile, proxyConf = {}) {
   }
   proxy = require(watchFile);
 
-  app.all('/*', function (req, res, next) {
-    const proxyURL = `${req.method} ${req.path}`;
-    const proxyNames = Object.keys(proxyConf);
-    const proxyFuzzyMatch = proxyNames.filter(function (kname) {
-      return /\*$/.test(kname) && (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(proxyURL);;
-    });
-    const proxyMatch = proxyNames.filter(function (kname) {
-      return kname === proxyURL;
-    });
-
-    if (proxyNames.length > 0 && (proxyMatch.length > 0 || proxyFuzzyMatch.length > 0)) {
-      let currentProxy = proxyNames.filter(function (kname) {
-        return (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(proxyURL);;
-      });
-      currentProxy = proxyConf[currentProxy[0]];
-      proxyHTTP.web(req, res, { target: currentProxy });
-    } else if (proxy[proxyURL]) {
-      let bodyParserMethd = bodyParser.json();
-      const contentType = req.get('Content-Type');
-      if (contentType === 'text/plain') {
-        bodyParserMethd = bodyParser.raw({ type: 'text/plain' });
-      }
-      bodyParserMethd(req, res, function () {
-        if (typeof proxy[proxyURL] === 'function') {
-          proxy[proxyURL](req, res, next);
-        } else {
-          res.json(proxy[proxyURL]);
-        }
-      });
-    } else {
+  if (!proxy) {
+    return function (req, res, next) {
       next();
     }
-  });
+  }
 
   // 监听文件修改重新加载代码
   // 配置热更新
@@ -57,11 +30,52 @@ module.exports = function (app, watchFile, proxyConf = {}) {
       cleanCache(require.resolve(watchFile));
       // 完事儿，进行赋值，哈哈成功了！
       proxy = require(watchFile);
-      console.log(`Hot Mocker ${watchFile} file replacement success!`);
+      console.log(`${` Done: `.green_b.black} Hot Mocker ${watchFile.replace(__dirname,'')} file replacement success!`);
     } catch (ex) {
-      console.error('Hot Mocker file replacement failed!!');
+      console.error(`${` Failed: `.green_b.black} Hot Mocker file replacement failed!!`);
     }
   });
+
+  app.all('/*', function (req, res, next) {
+    const proxyURL = `${req.method} ${req.path}`;
+    const proxyNames = Object.keys(proxyConf);
+    const proxyFuzzyMatch = proxyNames.filter(function (kname) {
+      return /\*$/.test(kname) && (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(proxyURL);;
+    });
+    const proxyMatch = proxyNames.filter(function (kname) {
+      return kname === proxyURL;
+    });
+    // 判断下面这种情况的路由
+    // => GET /api/user/:org/:name
+    const containMockURL = Object.keys(proxy).filter(function (kname) {
+      return (new RegExp(kname.replace(/(:\w*)[^/]/ig, '(.*)'))).test(proxyURL);
+    });
+
+    if (proxyNames.length > 0 && (proxyMatch.length > 0 || proxyFuzzyMatch.length > 0)) {
+      let currentProxy = proxyNames.filter(function (kname) {
+        return (new RegExp("^" + kname.replace(/\/\*$/, ''))).test(proxyURL);;
+      });
+      currentProxy = proxyConf[currentProxy[0]];
+      proxyHTTP.web(req, res, { target: currentProxy });
+    } else if (proxy[proxyURL] || (containMockURL && containMockURL.length > 0)) {
+      let bodyParserMethd = bodyParser.json();
+      const contentType = req.get('Content-Type');
+      if (contentType === 'text/plain') {
+        bodyParserMethd = bodyParser.raw({ type: 'text/plain' });
+      }
+      bodyParserMethd(req, res, function () {
+        const result = proxy[proxyURL] || proxy[containMockURL[0]];
+        if (typeof result === 'function') {
+          result(req, res, next);
+        } else {
+          res.json(result);
+        }
+      });
+    } else {
+      next();
+    }
+  });
+
   // 释放老模块的资源
   function cleanCache(modulePath) {
     var module = require.cache[modulePath];

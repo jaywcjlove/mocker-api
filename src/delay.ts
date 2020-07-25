9f87000e69cce991787fbeaccb53e75eeb30c084
jaywcjlove <398188662@qq.com>
2020-07-25 23:50:53 +0800

    doc: Add type document.

--- src/delay.ts
+++ src/delay.ts
@@ -1,8 +1,26 @@
-import { Request, Response, NextFunction } from 'express';
-import { MockerProxyRoute, MockerResultFunction } from './';
+import { Request, Response } from 'express';
+import { MockerProxyRoute, MockerResult, MockerResultFunction } from './';
 
-export default function delay(proxy: MockerProxyRoute, timer: number = 0) {
-  const mockApi: { [key: string]: any } | ((req: Request, res: Response, next: NextFunction) => void) = {};
+/**
+ * You can use functional tool to enhance mock. [#17](https://github.com/jaywcjlove/webpack-api-mocker/issues/17)
+ * 
+ * ```js
+ * const delay = require('mocker-api/lib/delay');
+ * const noProxy = process.env.NO_PROXY === 'true';
+ * 
+ * const proxy = {
+ *   'GET /api/user': {
+ *     id: 1,
+ *     username: 'kenny',
+ *     sex: 6
+ *   },
+ *   // ...
+ * }
+ * module.exports = (noProxy ? {} : delay(proxy, 1000));
+ * ```
+ */
+export default function delay(proxy: MockerProxyRoute, timer: number = 0): MockerResult {
+  const mockApi: MockerResult = {};
   Object.keys(proxy).forEach((key) => {
     const result = proxy[key];
     if ((Object.prototype.toString.call(result) === '[object String]' && /^http/.test(result as string)) || key === '_proxy' || timer === 0) {
